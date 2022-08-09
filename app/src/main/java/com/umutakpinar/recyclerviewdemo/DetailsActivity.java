1e2a38b765f41575b94a78e76be4b214832a57c7
Umut Akpinar <umtakpnr5@gmail.com>
2022-08-09 15:22:05 +0300

    ViewBinding integrated Main & Details Activity

--- app/src/main/java/com/umutakpinar/recyclerviewdemo/DetailsActivity.java
+++ app/src/main/java/com/umutakpinar/recyclerviewdemo/DetailsActivity.java
@@ -3,12 +3,21 @@ package com.umutakpinar.recyclerviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.umutakpinar.recyclerviewdemo.databinding.ActivityDetailsBinding;
+import com.umutakpinar.recyclerviewdemo.databinding.ActivityMainBinding;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private ActivityDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
